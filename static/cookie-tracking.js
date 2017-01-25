88fbcd4e20a33bb4ee710e623eeca13cc95661e0
Oskar Johan Kala <oskar.kala@whitewizard.eu>
2017-01-25 16:07:19 +0200

    added cookie creator for testing phase

--- static/cookie-tracking.js
+++ static/cookie-tracking.js
@@ -1,16 +1,41 @@
 window.onload = initialize_sess_api();
 
 function initialize_sess_api() {
-    var user_nr_from_cookie = parseInt(document.cookie.split('.')[2]);
-
+    
     window.hosturl = 'http://localhost:5000';
+    if (getCookie('_ga') == undefined) {
+
+        var ga_id = prompt("Please enter a cookie:", 'GA1.2.37.1479217586');
+        if (ga_id != null) {
+            createCookie('_ga', ga_id, 1);
+        }
+    }
+
+    var ga_cookie = getCookie('_ga');
+
+    var user_nr_from_cookie = parseInt(ga_cookie.split('.')[2]);
 
     if (user_nr_from_cookie % 37 === 0) {
-        alert("ur cookie is bad");
         onLoadEvent();
         exitIntentListener();
         window.addEventListener("beforeunload", onUnLoadEvent);
     }
+}
+
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+function createCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + '=' + value + expires + "; path=/";
 }
 
 function onLoadEvent() {
@@ -28,9 +53,9 @@ function onLoadEvent() {
             window.exp_time = JSON.parse(xhttp.responseText).exp_time;
 
             setTimeout(userLeaving, window.user_leaving_in * 1000);
-            
-             if (window.visited == 1){
-                setTimeout(setVisitedTimer, window.exp_time*1000);
+
+            if (window.visited == 1) {
+                setTimeout(setVisitedTimer, window.exp_time * 1000);
             }
         }
     };
@@ -58,7 +83,7 @@ function exitIntentListener() {
             exitIntentEvent();
             alert("You left the window :)");
             window.visited = 1;
-            setTimeout(setVisitedTimer, window.exp_time*1000);
+            setTimeout(setVisitedTimer, window.exp_time * 1000);
         }
     });
 }
